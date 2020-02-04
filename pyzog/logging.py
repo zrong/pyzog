d86d2a113dc5de6b61042a243350a4741fed11b8
zrong <zrongzrong@gmail.com>
2020-02-04 18:49:21 +0800

    add redishandler

--- pyzog/logging.py
+++ pyzog/logging.py
@@ -15,7 +15,7 @@ from pythonjsonlogger import jsonlogger
 TEXT_LOG_FORMAT = """
 [%(asctime)s] %(levelname)s in %(module)s.%(funcName)s [%(pathname)s:%(lineno)d]:
 %(message)s"""
-JSON_LOG_FORMAT = r'%(levelname)s %(module)s %(funcName)s %(pathname)s %(lineno) %(threadName) %(processName) %(message)'
+JSON_LOG_FORMAT = r'%(levelname)s %(module)s %(funcName)s %(pathname)s %(lineno) %(threadName) %(processName) %(created) %(message)'
 
 
 class PUSHHandler(logging.Handler):
@@ -100,6 +100,34 @@ class SUBSCRIBEHandler(logging.Handler):
             self.handleError(record)
 
 
+class RedisHandler(logging.Handler):
+    """基于 Redis 的 publish 命令来发布 log
+    """
+    # redis 实例
+    r = None
+
+    # pubsub 实例
+    pub = None
+
+    # publish 频道
+    channel = None
+    
+    def __init__(self, url, channel, **kwargs):
+        logging.Handler.__init__(self)
+        self.channel = channel
+        self.r = redis.from_url(url, **kwargs)
+        self.pub = self.r.pubsub()
+
+    def emit(self, record):
+        """Emit a log message on redis."""
+        msg = self.format(record)
+        try:
+            self.pub.publish(msg)
+        except Exception:
+            self.pub.close()
+            self.handleError(record)
+
+
 def _create_file_handler(target, filename):
     """ 创建一个基于文件的 logging handler
     :param target: 一个 Path 对象，或者一个 path 字符串
@@ -126,20 +154,31 @@ def _create_zmq_handler(target):
     return PUSHHandler(target)
 
 
+def _create_redis_handler(target, channel):
+    """ 创建一个基于 zeromq 的 logging handler
+    :param target: 一个字符串，形如： tcp://127.0.0.1:8334
+    """
+    return RedisHandler(target, channel)
+
+
 def get_logging_handler(type_, fmt, level, target=None, name=None) :
     """ 获取一个 logger handler
 
-    :param type_: stream/file/zmq
+    :param type_: stream/file/zmq/redis
     :param fmt: raw/text/json raw 代表原样写入，不增加内容
     :param level: logging 的 level 级别
     :param target: 项目主目录的的 path 字符串或者 Path 对象，也可以是 tcp://127.0.0.1:8334 这样的地址
-    :param name: logger 的名称，不要带扩展名
+    :param name: logger 的名称，不要带扩展名，对于 type 为 redis 的 handler，name 代表 redis publish channel
     """
     handler = None
     if type_ == 'zmq':
         if target is None:
             raise TypeError('target is necessary if type is zmq!')
         handler = _create_zmq_handler(target)
+    elif type_ == 'redis':
+        if name is None:
+            raise TypeError('name is necessary if type is redis!')
+        handler = _create_redis_handler(target, name)
     elif type_ == 'file':
         if target is None or name is None:
             raise TypeError('target and name is necessary if type is file!')
@@ -162,7 +201,7 @@ def get_logger(name, target=None, type_='file', fmt='text', level=logging.INFO):
 
     :param name: logger 的名称，不要带扩展名
     :param target: 项目主目录的的 path 字符串或者 Path 对象，也可以是 tcp://127.0.0.1:8334 这样的地址
-    :param type_: stream/file/zmq
+    :param type_: stream/file/zmq/redis
     :param fmt: raw/text/json
     :param level: logging 的 level 级别
     """
